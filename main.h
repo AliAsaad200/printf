f6f775481a23205fda0f3d0398f914757e519efe
janaahmed77 <janaabdelmageed77@gmail.com>
2023-10-15 04:40:20 +0300

    hiiii

--- main.h
+++ main.h
@@ -3,6 +3,6 @@
 int _putchar(char c);
 void print_correct_string(const char *sttring_task);
 int _printf(const char *format, ...);
-int format_arguments(const char *format, va_list argument_format_output);
+int format_arguments_output(const char *format, va_list argument_format);
 int print_correct_int(int no);
 #endif
