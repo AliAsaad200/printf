25cbcc40e6b7ce0f033684642d3a213717c095bb
janaahmed77 <janaabdelmageed77@gmail.com>
2023-10-15 04:23:16 +0300

    hiiii

--- functions.c
+++ functions.c
@@ -2,19 +2,9 @@
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "main.h"
-/**
- * print_correct_string - Print a string.
- * @sttring_task: The string to print.
- */
-void print_correct_string(const char *sttring_task)
-{
-	while (*sttring_task)
-	{
-	_putchar(*sttring_task);
-	sttring_task++;
-	}
-}
+
 /**
  * print_correct_int - Print a num.
  * @no: The character to print.
@@ -52,77 +42,4 @@ int print_correct_int(int no)
 	char_count++;
 	}
 	return (char_count);
-}
-/**
- * format_arguments_output - Handle formatted output.
- * @format: The format string.
- * @argument_format_output: The variable arguments list.
- *
- * Return: The number of characters printed.
- */
-int format_arguments_output(const char *format, va_list argument_format_output)
-{
-	int counting_down = 0;
-	const char *c;
-	for (c = format; *c; ++c)
-	{
-	if (*c != '%')
-	{
-	_putchar(*c);
-	counting_down++;
-	}
-	else
-	{
-	c++;
-	switch (*c)
-	{
-	case 's':
-	{
-	char *sttring_task = va_arg(argument_format_output, char *);
-
-	print_correct_string(sttring_task);
-	counting_down += strlen(sttring_task);
-	break;
-	}
-	case 'c':
-	{
-	char c = va_arg(argument_format_output, int);
-
-	_putchar(c);
-	counting_down++;
-	break;
-	}
-	case 'i': case 'd':
-	{
-	int argument_numbs = va_arg(argument_format_output, int);
-
-	counting_down += print_correct_int(argument_numbs);
-	break;
-	}
-	default:
-	_putchar('%');
-	_putchar(*c);
-	counting_down += 2;
-	break;
-	}
-	}
-	}
-	return (counting_down);
-}
-/**
- * _printf - Custom printf function.
- * @format: The format string.
- * @...: Variable arguments.
- *
- * Return: The number of characters printed.
- */
-int _printf(const char *format, ...)
-{
-	int result;
-	va_list args;
-
-	va_start(args, format);
-	result = format_arguments_output(format, args);
-	va_end(args);
-	return (result);
 }
