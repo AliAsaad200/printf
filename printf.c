2fd5c6b5a8cf67cfe754f6925b784fdba1fad4a3
AliAsaad200 <aliahmedassad2003@gmail.com>
2023-10-15 04:23:55 +0300

    really

--- printf.c
+++ printf.c
@@ -15,51 +15,57 @@ void print_correct_string(const char *sttring_task)
 	}
 }
 /**
- * format_arguments_output - Handle formatted output.
+ * format_arguments_output0 - Handle formatted output.
  * @format: The format string.
  * @argument_format_output: The variable arguments list.
  *
  * Return: The number of characters printed.
  */
-int format_arguments_output(const char *format, va_list argument_format_output)
+  int format_arguments_output(const char *format, va_list argument_format_output)
 {
 	int counting_down = 0;
 	const char *c;
-
 	for (c = format; *c; ++c)
 	{
 	if (*c != '%')
 	{
-		_putchar(*c);
-		counting_down++;
+	_putchar(*c);
+	counting_down++;
 	}
 	else
 	{
-		c++;
-		switch (*c)
-		{
-		case 's':
-		{
-			char *sttring_task = va_arg(argument_format_output, char *);
+	c++;
+	switch (*c)
+	{
+	case 's':
+	{
+	char *sttring_task = va_arg(argument_format_output, char *);
 
-			print_correct_string(sttring_task);
-			counting_down += strlen(sttring_task);
-			break;
-		}
-		case 'c':
-		{
-			char c = va_arg(argument_format_output, int);
+	print_correct_string(sttring_task);
+	counting_down += strlen(sttring_task);
+	break;
+	}
+	case 'c':
+	{
+	char c = va_arg(argument_format_output, int);
 
-			_putchar(c);
-			counting_down++;
-			break;
-		}
-		default:
-			_putchar('%');
-			_putchar(*c);
-			counting_down += 2;
-		break;
-		}
+	_putchar(c);
+	counting_down++;
+	break;
+	}
+	case 'i': case 'd':
+	{
+	int argument_numbs = va_arg(argument_format_output, int);
+
+	counting_down += print_correct_int(argument_numbs);
+	break;
+	}
+	default:
+	_putchar('%');
+	_putchar(*c);
+	counting_down += 2;
+	break;
+	}
 	}
 	}
 	return (counting_down);
