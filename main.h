31533119b7b23acb6895c47531d1e2c82a5b28dc
janaahmed77 <janaabdelmageed77@gmail.com>
2023-10-18 01:25:39 +0300

    hhhh

--- main.h
+++ main.h
@@ -6,10 +6,7 @@ int _printf(const char *format, ...);
 int format_arguments_output(const char *format, va_list argument_format);
 int print_correct_int(int no);
 int format_arguments_output(const char *format, va_list argument_format);
-int process_plain_characters(const char *format, int *counting_down);
 void process_string_argument(va_list argument_format, int *counting_down);
-void process_char_argument(va_list argument_format, int *counting_down);
-void process_int_argument(va_list argument_format, int *counting_down);
 void process_unknown_specifier(const char *format, int *counting_down);
 
 #endif
